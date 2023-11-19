8379bd110950bdacdb9b32f0165f8ab365aca1fe
Mustapha Shuaibu <119011957+Mplanner12@users.noreply.github.com>
2023-11-19 21:51:08 +0100

    Bugs fix

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -10,22 +10,22 @@ export async function POST(request: Request) {
   const cartDetails = await request.json()
   const lineItems = validateCartItems(inventory, cartDetails)
   const origin = request.headers.get("origin")
-  await stripe.checkout.sessions.create({
+  const session = await stripe.checkout.sessions.create({
     submit_type: "pay",
     mode: "payment",
     payment_method_types: ["card"],
     line_items: lineItems,
     shipping_address_collection: {
-      allowed_countries: ["US", "AM", "CO", "BT", "SA"],
+      allowed_countries: ["US"],
     },
     shipping_options: [
       {
-        shipping_rate: "shr_10EHLADhyhl9j6LNYbNvfhjQ",
+        shipping_rate: "shr_1OEHvJDhyhl9j6LNSla3oTBF",
       },
     ],
     billing_address_collection: "auto",
     success_url: `${origin}/success?session_id={CHECKOUT_SESSION_ID}`,
     cancel_url: `${origin}/cart`,
   })
-  return NextResponse.json(Session)
+  return NextResponse.json(session)
 }
