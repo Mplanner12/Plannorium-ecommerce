d86f785ec45795a939d3fb61b43b295edbbae567
Mustapha Shuaibu <119011957+Mplanner12@users.noreply.github.com>
2023-11-30 02:28:15 +0100

    Image cartelogue created

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 ## E-Commerce App
-
-- [Project Link](https://bit.ly/fs-ecommerce)
-  --background: 224 71% 4%;
-  --foreground: 213 31% 91%;
--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -32,9 +32,7 @@ export default async function Page({ params }: Props) {
   return (
     <main className="mx-auto max-w-5xl sm:px-6 sm:pt-16 lg:px-8">
       <div className="mx-auto max-w-2xl lg:max-w-none">
-        {/* Product */}
         <div className="pb-20 lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-12">
-          {/* Product gallery */}
           <ProductGallery product={product} />
           <ProductInfo product={product} />
         </div>
